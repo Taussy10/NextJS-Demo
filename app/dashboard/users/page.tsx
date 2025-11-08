14e81ecc9a8f8163baa75c9a4a6c5682d31fa8c2
Tausif <tausif00100@gmail.com>
2025-11-08 19:35:13 +0530

    learned about client side and server side components

---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Users = () => {
+  return (
+    <div>
+      Dashobard Users
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   /* config options here */
+  // Expirmental means that it's a expirmental feature and they are
+  // improving it so you use it at your own risk.
+  experimental: {
+    // Firstly understand about Turbopack - our NextJS builder
+
+    // 1. Without turbopackFileSystemCacheForDev
+    // When we start server by npm run dev
+    // then it will build
+    // when we will make changes in code then it will build only that code(not from scratch)
+    // then we close the server
+    // then we start server
+    // Now it will build from scratch
+
+    // 2. With turbopackFileSystemCacheForDev
+    // When we start server by npm run dev
+    // then it will build
+    // when we will make changes in code then it will build only that code not from scratch
+    // then we close the server
+    // but it will save in memory
+    // then we start server
+    // Now it will build where we left in local disk not from scratch
+
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
